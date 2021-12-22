6a83ae986f156d611d304577ffdd5108cc8e7cf9
rafael-mingossi <rafaelmingossi@gmail.com>
2021-12-22 22:58:50 +1000

    final fix

--- components/ImagePoster/index.js
+++ components/ImagePoster/index.js
@@ -4,18 +4,49 @@ import styles from './styles.module.scss';
 const ImagePoster = ({ id }) => {
   return (
     <div className={styles.imagePoster}>
-      {id == 1 && <Image src={'/new-hope.jpg'} width={200} height={300} />}
-      {id == 2 && (
-        <Image src={'/empire-strikes.jpg'} width={200} height={300} />
+      {id == 1 && (
+        <Image src={'/new-hope.jpg'} alt="poster 1" width={200} height={300} />
       )}
-      {id == 3 && <Image src={'/return-jedhi.jpg'} width={200} height={300} />}
+      {id == 2 && (
+        <Image
+          src={'/empire-strikes.jpg'}
+          alt="poster 2"
+          width={200}
+          height={300}
+        />
+      )}
+      {id == 3 && (
+        <Image
+          src={'/return-jedhi.jpg'}
+          alt="poster 3"
+          width={200}
+          height={300}
+        />
+      )}
       {id == 4 && (
-        <Image src={'/phantom-menance.jpg'} width={200} height={300} />
+        <Image
+          src={'/phantom-menance.jpg'}
+          alt="poster 4"
+          width={200}
+          height={300}
+        />
       )}
       {id == 5 && (
-        <Image src={'/attack-of-clones.jpg'} width={200} height={300} />
+        <Image
+          src={'/attack-of-clones.jpg'}
+          alt="poster 5"
+          width={200}
+          height={300}
+        />
       )}
-      {id == 6 && <Image src={'/revenge-sith.jpg'} width={200} height={300} />}
+      {id == 6 && (
+        <Image
+          src={'/revenge-sith.jpg'}
+          alt="poster 6"
+          width={200}
+          height={300}
+        />
+      )}
     </div>
   );
 };
